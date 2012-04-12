916ec5b46aa9a6091350c8463b70cd0d8dba5d8a
Beth Finnegan <efinnegan@tresys.com>
2012-04-12 15:43:05 -0400

    bug-15667: Adding JALException class

---
+++ src/main/java/com/tresys/jalop/common/JALException.java
@@ -0,0 +1,34 @@
+/**
+ * Custom Exception class for JALoP specific exceptions.
+ * <p>
+ * Source code in 3rd-party is licensed and owned by their respective
+ * copyright holders.
+ * <p>
+ * All other source code is copyright Tresys Technology and licensed as below.
+ * <p>
+ * Copyright (c) 2012 Tresys Technology LLC, Columbia, Maryland, USA
+ * <p>
+ * This software was developed by Tresys Technology LLC
+ * with U.S. Government sponsorship.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tresys.jalop.common;
+
+public class JALException extends Exception {
+
+	public JALException(String msg) {
+		super(msg);
+	}
+}
